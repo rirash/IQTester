7f4f38cedfa4ba0503c6eb3b25997199b43620e1
Анастасия <@HOME-PC>
2021-04-08 17:05:15 +0400

    InitReq

---
+++ Models/UserClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IQ_MOTIVATION_TEST.Models
+{
+    class UserClass
+    {
+        private string _identificationNumber;
+        private int _numberTest;
+        private double[] _resultsTest;
+
+        public UserClass(string ident_number, string numb_test)
+        {
+            _identificationNumber = ident_number;
+            if (numb_test == "Интеллектуальные характеристики")
+            {
+                _numberTest = 1;
+                _resultsTest = new double[4];
+            }
+            else
+            {
+                _numberTest = 2;
+                _resultsTest = new double[8];
+            }
+        }
+    }
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IQ_MOTIVATION_TEST.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
